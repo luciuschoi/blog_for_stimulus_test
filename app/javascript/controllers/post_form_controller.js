91455307cebb13c3ea257f883f294c36b7c4fdfc
Lucius Choi <lucius.choi@gmail.com>
2019-04-30 12:01:37 +0900

    added focus and blur event on post title input

--- app/javascript/controllers/post_form_controller.js
+++ app/javascript/controllers/post_form_controller.js
@@ -1,9 +1,29 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-    static targets: []
+    static targets = [ "postTitle", "count"]
+
+    connect() {
+        this.countChars()
+    }
 
     countChars() {
-        this.
+        // console.log(this.postTitleTarget.value)
+        let title = this.postTitleTarget
+        let count = title.value.length
+        if(count === 1){
+            this.countTarget.textContent = '1 character'
+        } else {
+            this.countTarget.textContent = `${count} characters`
+        }
+        // this.postTitleTarget.classList.add('text-yellow')
+    }
+
+    focusing() {
+        this.postTitleTarget.classList.add('text-yellow')
+    }
+
+    blurring() {
+        this.postTitleTarget.classList.remove('text-yellow')
     }
 }
